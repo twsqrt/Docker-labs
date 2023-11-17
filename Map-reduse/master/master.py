254e16b96dd164bae7d9224f996d41c8161a8b17
twsqrt <twsqrt@gmail.com>
2023-11-17 11:35:35 +0300

    Added memory dump

--- Map-reduse/master/master.py
+++ Map-reduse/master/master.py
@@ -1,11 +1,15 @@
 import socket
+import os
+import glob
 import json
 import sys
 import time 
 
 RECV_BUFF_SIZE = 100 * 1024
+MAX_WORDS_RATE_DICT_SIZE = 100
 NUMBER_OF_TOP_LIST = 30
-
+NUMBER_OF_DATA_FILES = 10
+DATA_PATH_FORMAT = 'data/{0}.txt'
 
 class Slave:
     def __init__(self, address: str):
@@ -39,7 +43,7 @@ class Slave:
             data = self.__socket.recv(RECV_BUFF_SIZE)
         except BlockingIOError:
             return False
-        self.__current_result_dict =  json.loads(data.decode('utf-8'))
+        self.__current_result_dict = json.loads(data.decode('utf-8'))
         self.__has_result = True
         self.__is_working = False
         return True
@@ -66,6 +70,35 @@ def add_words_rate(to_dict: dict, from_dict: dict) -> None:
         else:
             to_dict[word] = count
 
+def split_words_rate_by_hash(words_rate_dict: dict) -> dict:
+    words_rate_by_hash = {}
+    for i in range(0, NUMBER_OF_DATA_FILES):
+        words_rate_by_hash[i] = {}
+    for word, rate in words_rate_dict.items():
+        file_id = hash(word) % NUMBER_OF_DATA_FILES
+        words_rate_by_hash[file_id][word] = rate
+    return words_rate_by_hash
+
+def dump_wrods_rate(words_rate_dict: dict) -> None:
+    words_rate_by_hash = split_words_rate_by_hash(words_rate_dict)
+    for file_id, words_rate in words_rate_by_hash.items():
+        file_words_rate = {}
+        with open(DATA_PATH_FORMAT.format(file_id), 'r') as file:
+            for line in file:
+                word, rate_str = line.split(' ')
+                file_words_rate[word] = int(rate_str)
+        add_words_rate(file_words_rate, words_rate)
+        with open(DATA_PATH_FORMAT.format(file_id), 'w') as file:
+            for word, rate in sorted(file_words_rate.items(), key=lambda x: x[1], reverse=True):
+                file.write('{w} {r}\n'.format(w=word, r=rate))
+
+def get_file_top_words_rate(file_id: int, number_of_top: int) -> dict:
+    with open(DATA_PATH_FORMAT.format(file_id), 'r') as file:
+        file_top_words_rate = {}
+        for _ in range(0, number_of_top):
+            word, rate_str = next(file).split(' ')
+            file_top_words_rate[word] = int(rate_str)
+        return file_top_words_rate
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -85,6 +118,12 @@ if __name__ == '__main__':
             break
         pages.append(line)
 
+    for file in glob.glob('data/*.txt'):
+        os.remove(file)
+
+    for i in range(0, NUMBER_OF_DATA_FILES):
+        open(DATA_PATH_FORMAT.format(i), 'a').close()    
+
     while len(pages) > 0:
         for slave in slaves:
             slave.status_update()
@@ -95,6 +134,10 @@ if __name__ == '__main__':
                 page = pages.pop()
                 slave.send_page(page)
                 print(f'send page: {page}')
+        if len(words_rate_dict) > MAX_WORDS_RATE_DICT_SIZE:
+            print(f'created memory dump!')
+            dump_wrods_rate(words_rate_dict)
+            words_rate_dict.clear()
         time.sleep(0.2)
         
     while len(slaves) > 0:
@@ -105,9 +148,17 @@ if __name__ == '__main__':
                 add_words_rate(words_rate_dict, result_dict)
                 slave.close()
                 slaves.remove(slave)
+        if len(words_rate_dict) > MAX_WORDS_RATE_DICT_SIZE:
+            print(f'created memory dump!')
+            dump_wrods_rate(words_rate_dict)
+            words_rate_dict.clear()
         time.sleep(0.2)
 
-    print('result:')
-    sorted_word_rate_dict = sorted(words_rate_dict.items(), key=lambda x: x[1], reverse=True) 
-    for word, count in sorted_word_rate_dict[:NUMBER_OF_TOP_LIST]:
-        print(f'{word}\t matches:{count}')
+    top_words_rate = {}
+    for file_id in range(0, NUMBER_OF_DATA_FILES):
+        file_top_words_rate = get_file_top_words_rate(file_id, NUMBER_OF_TOP_LIST)
+        add_words_rate(top_words_rate, file_top_words_rate)
+        sorted_words_rate_list = sorted(top_words_rate.items(), key=lambda x: x[1], reverse=True)
+        top_words_rate = dict(sorted_words_rate_list[:NUMBER_OF_TOP_LIST])
+    
+    print(top_words_rate)
