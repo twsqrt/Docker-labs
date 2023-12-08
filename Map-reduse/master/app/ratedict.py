23af9784444c692e135b129b3778f54a30a22a2d
twsqrt <twsqrt@gmail.com>
2023-12-08 14:59:35 +0300

    changed ratedict

--- Map-reduse/master/app/ratedict.py
+++ Map-reduse/master/app/ratedict.py
@@ -1,28 +1,15 @@
 from __future__ import annotations
-import json
 
 class RateDict(dict):
     def __init__(self, rate: dict = {}):
-        self.__rate = rate
+        self.update(rate)
 
-    def __len__(self) -> int:
-        return len(self.__rate)
-
-    def __contains__(self, key) -> bool:
-        return key in self._rate
-    
-    def add(self, key, rate: int) -> None:
-        if key in self.__rate:
-            self.__rate[key] += rate
+    def add_rate(self, key, rate: int) -> None:
+        if key in self:
+            self[key] += rate
         else:
-            self.__rate[key] = rate
+            self[key] = rate
     
-    def add_dict(self, rate_dict: dict) -> None:
+    def add_rate_dict(self, rate_dict: dict) -> None:
         for key, rate in rate_dict.items():
-            self.add(key, rate)
-    
-    def clear(self) -> None:
-        self.__rate.clear()
-
-    def items(self) -> dict:
-        return self.__rate.items()
+            self.add_rate(key, rate)
